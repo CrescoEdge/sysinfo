1f051d2ce4ed7d8dbfe8bed38d85b73d382d93cb
Cody Bumgardner <cody@uky.edu>
2019-01-05 21:09:13 -0500

    Put sysinfo on data plane

--- src/main/java/io/cresco/sysinfo/PerfSysMonitor.java
+++ src/main/java/io/cresco/sysinfo/PerfSysMonitor.java
@@ -1,10 +1,10 @@
 package io.cresco.sysinfo;
 
 
-import io.cresco.library.messaging.MsgEvent;
 import io.cresco.library.plugin.PluginBuilder;
 import io.cresco.library.utilities.CLogger;
 
+import javax.jms.MapMessage;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -36,19 +36,6 @@ public class PerfSysMonitor {
         if (this.running) return this;
         Long interval = plugin.getConfig().getLongParam("perftimer", 5000L);
 
-        /*
-        MsgEvent initial = new MsgEvent(MsgEvent.Type.INFO, plugin.getRegion(), plugin.getAgent(), plugin.getPluginID(), "Performance Monitoring timer set to " + interval + " milliseconds.");
-        initial.setParam("src_region", plugin.getRegion());
-        initial.setParam("src_agent", plugin.getAgent());
-        initial.setParam("src_plugin", plugin.getPluginID());
-        initial.setParam("dst_region", plugin.getRegion());
-        initial.setParam("dst_agent", plugin.getAgent());
-        initial.setParam("dst_plugin", plugin.getPluginID());
-        initial.setParam("is_regional",Boolean.TRUE.toString());
-        initial.setParam("is_global",Boolean.TRUE.toString());
-        plugin.sendMsgEvent(initial);
-        */
-
         timer = new Timer();
         timer.scheduleAtFixedRate(new PerfMonitorTask(plugin), 500L, interval);
         return this;
@@ -74,37 +61,43 @@ public class PerfSysMonitor {
             this.plugin = plugin;
         }
 
+
         public void run() {
 
-            MsgEvent tick = plugin.getKPIMsgEvent();
+            try {
+                MapMessage mapMessage = plugin.getAgentService().getDataPlaneService().createMapMessage();
+
+                if (plugin.getConfig().getBooleanParam("benchmark", false)) {
+                    mapMessage.setDouble("benchmark_cpu_composite", bm.getCPU());
+                }
+
+                mapMessage.setString("perf",builder.getSysInfoMap());
+
+                //set property
+                mapMessage.setStringProperty("pluginname",plugin.getConfig().getStringParam("pluginname"));
+                mapMessage.setStringProperty("region_id",plugin.getRegion());
+                mapMessage.setStringProperty("agent_id",plugin.getAgent());
+                mapMessage.setStringProperty("plugin_id", plugin.getPluginID());
+
+
+            } catch (Exception ex) {
+                logger.error(ex.getMessage());
+                ex.printStackTrace();
+            }
+
             /*
-            MsgEvent tick = new MsgEvent(MsgEvent.Type.KPI, plugin.getRegion(), plugin.getAgent(), plugin.getPluginID(), "Performance Monitoring tick.");
-            tick.setParam("src_region", plugin.getRegion());
-            tick.setParam("src_agent", plugin.getAgent());
-            tick.setParam("src_plugin", plugin.getPluginID());
-            tick.setParam("dst_region", plugin.getRegion());
-            tick.setParam("dst_agent", plugin.getAgent());
-            tick.setParam("dst_plugin", "plugin/0");
-            tick.setParam("is_regional",Boolean.TRUE.toString());
-            tick.setParam("is_global",Boolean.TRUE.toString());
-            */
-
-
+            MsgEvent tick = plugin.getKPIMsgEvent();
             tick.setParam("resource_id",plugin.getConfig().getStringParam("resource_id","sysinfo_resource"));
             tick.setParam("inode_id",plugin.getConfig().getStringParam("inode_id","sysinfo_inode"));
 
             if(plugin.getConfig().getBooleanParam("benchmark",false)) {
                 tick.setParam("benchmark_cpu_composite",String.valueOf((int)bm.getCPU()));
             }
-
             tick.setCompressedParam("perf",builder.getSysInfoMap());
-            /*
-            for(Map.Entry<String, String> entry : builder.getSysInfoMap().entrySet()) {
-                tick.setParam(entry.getKey(), entry.getValue());
-            }
+            plugin.msgOut(tick);
             */
 
-            plugin.msgOut(tick);
+
         }
     }
 }
